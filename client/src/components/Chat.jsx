25c590b8df6356a6341fb9786c3acee23da94f27
Sal Ceja <salceja45@gmail.com>
2023-12-05 16:04:01 -0800

    Header component layout

---
+++ client/src/components/Chat.jsx
@@ -0,0 +1,1 @@
+<div>Chat Component</div>;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,62 @@
+import AuthService from "../utils/auth";
+
+const dropdownOptions = ["LA", "San Diego", "Inland Empire"];
+
+const handleOptionChange = (e) => {
+  const selectedOption = e.target.value;
+  false && console.log(selectedOption);
+  /*  
+-   if (selectedOption === "LA") {
+      
+    } else if (selectedOption === "San Diego") {
+      
+    } else if (selectedOption === "Inland Empire") {
+      
+    }
+
+- need to load the new station base on the option selected.
+
+*/
+};
+const toLoginPage = () => {
+  window.location.href = "/login";
+};
+const toSignupPage = () => {
+  window.location.href = "/signup";
+};
+const toLogOutPage = () => {
+  AuthService.logout();
+  window.location.href = "/logout";
+};
+
+const Header = () => {
+  //{ isLoggedIn, login, logout, register }
+
+  return (
+    <header>
+      <div style={{ marginLeft: "10px" }}>
+        <select onChange={handleOptionChange}>
+          {dropdownOptions.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
+      <h1>Radioheadz</h1>
+      <p>Listen add free from anywhere in the country.</p>
+      {AuthService.loggedIn() ? (
+        <div>
+          <button onClick={toLogOutPage}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <button onClick={toLoginPage}>Login</button>
+          <button onClick={toSignupPage}>Signup</button>
+        </div>
+      )}
+    </header>
+  );
+};
+
+export default Header;
