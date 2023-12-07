b32d23e8768b30b2e33d4025a2e4cb2aa5849ef4
Sal Ceja <salceja45@gmail.com>
2023-12-07 11:58:03 -0800

    added the change station buttons

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -7,61 +7,78 @@ const Carousel = () => {
         data-bs-ride="carousel"
         data-bs-interval="2000"
       >
-        <div className='carousel-inner'>
-          <div className='carousel-item active'>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
             <img
-              src='../../src/assets/images/Los-Angeles.png'
-              className='d-block'
-              alt='...'
-              style={{ width: '300px', height: '300px' }}
+              src="../../src/assets/images/Los-Angeles.png"
+              className="d-block"
+              alt="..."
+              style={{ width: "300px", height: "300px" }}
             />
           </div>
-          <div className='carousel-item'>
+          <div className="carousel-item">
             <img
-              src='../../src/assets/images/radio_app.webp'
-              className='d-block'
-              alt='...'
-              style={{ width: '300px', height: '300px' }}
+              src="../../src/assets/images/radio_app.webp"
+              className="d-block"
+              alt="..."
+              style={{ width: "300px", height: "300px" }}
             />
           </div>
-          <div className='carousel-item'>
+          <div className="carousel-item">
             <img
-              src='../../src/assets/images/sandiegoSidebar.png'
-              className='d-block ' // w-200
-              alt='...'
-              style={{ width: '300px', height: '300px' }}
+              src="../../src/assets/images/sandiegoSidebar.png"
+              className="d-block " // w-200
+              alt="..."
+              style={{ width: "300px", height: "300px" }}
             />
           </div>
         </div>
         <button
-          className='carousel-control-prev'
-          type='button'
-          data-bs-target='#carouselExampleRide'
-          data-bs-slide='prev'
-          style={{ fontWeight: 'bold', color: 'black' }}
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleRide"
+          data-bs-slide="prev"
+          style={{ fontWeight: "bold", color: "black" }}
         >
           <span
-            className='carousel-control-prev-icon'
-            aria-hidden='true'
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
           ></span>
-          <span className='visually-hidden'>Previous</span>
+          <span className="visually-hidden">Previous</span>
         </button>
         <button
-          className='carousel-control-next'
-          type='button'
-          data-bs-target='#carouselExampleRide'
-          data-bs-slide='next'
-          style={{ fontWeight: 'bold', color: 'black' }}
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleRide"
+          data-bs-slide="next"
+          style={{ fontWeight: "bold", color: "black" }}
         >
           <span
-            className='carousel-control-next-icon'
-            aria-hidden='true'
+            className="carousel-control-next-icon"
+            aria-hidden="true"
           ></span>
-          <span className='visually-hidden'>Next</span>
+          <span className="visually-hidden">Next</span>
         </button>
       </div>
       <div>
-        <h2>Featured Stations</h2>
+        <h2 className="d-flex justify-content-center">Featured Stations</h2>
+      </div>
+      <div id="stationButtons" className="d-flex justify-content-center">
+        <button id="prevStation" className="btn btn-secondary mx-2">
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+        </button>
+        <button id="playStation" className="btn btn-secondary mx-2">
+          Play
+        </button>
+        <button id="nextStation" className="btn btn-secondary mx-2">
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+        </button>
       </div>
     </>
   );
