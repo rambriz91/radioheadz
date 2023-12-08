cef41d4a3f6e6f0b1e4708e891047f5a0fbcedbd
rambriz91 <rambriz91@gmail.com>
2023-12-08 13:35:52 -0800

    got the tuner to populate stations.

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -1,20 +1,14 @@
-
 import Tuner from './Tuner';
-import { useQuery } from "@apollo/client";
-import { QUERY_STATIONS } from "../utils/queries";
+import { useQuery } from '@apollo/client';
+import { QUERY_STATIONS } from '../utils/queries';
 const Body = () => {
   const { loading, data } = useQuery(QUERY_STATIONS);
-  console.log(data)
   const stations = data?.stations || [];
   return (
     <div style={{ display: 'flex' }}>
-      Body
       <div>
-        {loading ? (<div> Loading...</div>) :
-          (<Tuner
-            stations={stations} />)}
+        {loading ? <div> Loading...</div> : <Tuner stations={stations} />}
       </div>
-    
     </div>
   );
 };
--- client/src/components/Tuner.jsx
+++ client/src/components/Tuner.jsx
@@ -1,28 +1,69 @@
+import { useState } from 'react';
+import DropDown from './DropDown';
 
-import PlayerControls from "./Controls";
-import TunerBar from "./TunerBar";
+const Tuner = ({ stations }) => {
+  const [freq, setFreq] = useState(92.5);
+  const [currentStation, setCurrentStation] = useState(null);
+  //const [currentCity, setCurrentCity] = useState('');
 
+  //const filterStations = stations.filter((station) => station.city);
 
-const Tuner = (stations) => {
-console.log(stations)
+  const stationData = {};
+  stations.forEach((station) => {
+    const { frequency, ...rest } = station;
+    stationData[frequency] = rest;
+  });
+
+  const handleTuner = (event) => {
+    const newFreq = parseFloat(event.target.value);
+    setFreq(newFreq);
+
+    const station = stationData[newFreq];
+    setCurrentStation(station);
+    console.log(station);
+  };
+
   return (
-    <div className="flex flex-col items-center" >
-      <div id="radio-container">
-        <h2 id="name">{stations.name}</h2>
-        <div id="frequency">
-          <h2>{stations.frequency}</h2>
-        </div>
-        <h3 id="station">{stations.callSign}</h3>
-        <h4 id="genre">{stations.genre}</h4>
-        <audio id="audio" controls src="{stations.url}"></audio>
+    <div className='container flex flex-col items-center'>
+      <DropDown />
+      <h2 id='name'>{currentStation ? currentStation.name : ''}</h2>
+      <div id='frequency'>
+        <h2>{freq}</h2>
       </div>
-      <TunerBar />
-      <PlayerControls />
+      <h3 id='station'>{currentStation ? currentStation.callSign : ''}</h3>
+      <h4 id='genre'>{currentStation ? currentStation.genre : ''}</h4>
+      <audio
+        id='audio'
+        controls
+        src={currentStation ? currentStation.url : ''}
+      ></audio>
+      <div id='tuner-container'>
+        <input
+          id='tuner'
+          value={freq}
+          onInput={handleTuner}
+          step='0.1'
+          type='range'
+          min='88.0'
+          max='108.0'
+          list='markers'
+        />
+        <datalist id='markers'>
+          <option value='88.0'>88.0</option>
+          <option value='90.0'>90.0</option>
+          <option value='92.0'>92.0</option>
+          <option value='94.0'>94.0</option>
+          <option value='96.0'>96.0</option>
+          <option value='98.0'>98.0</option>
+          <option value='100.0'>100.0</option>
+          <option value='102.0'>102.0</option>
+          <option value='104.0'>104.0</option>
+          <option value='106.0'>106.0</option>
+          <option value='108.0'>108.0</option>
+        </datalist>
+      </div>
     </div>
   );
 };
 
 export default Tuner;
-
-
-
