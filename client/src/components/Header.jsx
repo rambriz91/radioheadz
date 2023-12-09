51852db01dcf2a27c4560f010e6400367d5aff0e
Jorge Gonzalez <jorge.1014@gmail.com>
2023-12-09 14:53:28 -0800

    cleaned up header component removed extra code

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,73 +1,7 @@
-// import AuthService from '../utils/auth';
-
-// const dropdownOptions = ['LA', 'San Diego', 'Inland Empire'];
-
-// const handleOptionChange = (e) => {
-//   const selectedOption = e.target.value;
-//   false && console.log(selectedOption);
-//   /*  
-// -   if (selectedOption === "LA") {
-
-//     } else if (selectedOption === "San Diego") {
-
-//     } else if (selectedOption === "Inland Empire") {
-
-//     }
-
-// - need to load the new station base on the option selected.
-
-// */
-// };
-// const toLoginPage = () => {
-//   window.location.href = '/login';
-// };
-// const toSignupPage = () => {
-//   window.location.href = '/signup';
-// };
-// const toLogOutPage = () => {
-//   AuthService.logout();
-//   window.location.href = '/login';
-// };
-
-// const Header = () => {
-//   //{ isLoggedIn, login, logout, register }
-
-//   return (
-//     <header>
-//       <div style={{ marginLeft: '10px' }}>
-//         <select onChange={handleOptionChange}>
-//           {dropdownOptions.map((option, index) => (
-//             <option key={index} value={option}>
-//               {option}
-//             </option>
-//           ))}
-//         </select>
-//       </div>
-//       <h1 style={{ color: "white" }}>Radioheadz</h1>
-//       <p style={{ color: "white" }}>
-//         {/* Changed add free to ad free */}
-//         Listen ad free from anywhere in the country. 
-//       </p>
-//       {AuthService.loggedIn() ? (
-//         <div>
-//           <button onClick={toLogOutPage}>Logout</button>
-//         </div>
-//       ) : (
-//         <div>
-//           <button onClick={toLoginPage}>Login</button>
-//           <button onClick={toSignupPage}>Signup</button>
-//         </div>
-//       )}
-//     </header>
-//   );
-// };
-
-// export default Header;
-
-
 import Logo from '../assets/images/radioheadz-high-resolution-logo-transparent.png';
 import AuthService from '../utils/auth';
 import { Link } from 'react-router-dom';
+
 const toLoginPage = () => {
   window.location.href = '/login';
 };
@@ -102,7 +36,7 @@ const Header = () => {
                 </Link>
               </li>
             </ul>
-            {/* <button className='border-2 px-6 py-3 my-2 flex items-center' onClick={toLogOutPage}>Logout</button> */}
+
           </div>
         ) : (
           <div className='w-[160px] h-[30px] flex justify-between list-none'>
@@ -118,9 +52,7 @@ const Header = () => {
                 </Link>
               </li>
             </ul>
-            {/* <button className='group border-1 px-1 py-1 my-1 flex  hover:bg-[#CA3433]' onClick={toLoginPage}>Login</button>
-            <button className='group border-1 px-1 py-1 my-1 flex  hover:bg-[#CA3433]' onClick={toSignupPage}>Signup</button>
-            */}
+
           </div>
         )}
       </div>
