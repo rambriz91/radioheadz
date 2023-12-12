d395346dd3e9f9792ac65c753e722efc040623de
WeenoCoding <hernandez17marc@gmail.com>
2023-12-11 20:24:17 -0800

    added users to data, fixed footer problem on home page

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,13 +1,38 @@
 import Profile from '../components/Profile';
 
 const ProfilePage = () => {
+
+    const styles = {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '75vh'
+    };
+    const h1 = {
+        display: 'flex',
+        justifyContent: 'center',
+        color: '#ffa62e',
+        fontSize: 75
+    };
+    const p = {
+        display: 'flex',
+        justifyContent: 'center',
+        color: '#ffa62e',
+        fontSize: 25
+    };
+    const chat = {
+        display: 'flex',
+        justifyContent: 'right',
+        color: '#ffa62e',
+        fontSize: 25,
+        marginRight: 450
+    }
     return (
-        <div>
-            <h1>Welcome to your profile page</h1>
-            <div>
+        <div style={styles}>
+            <h1 style={h1}>Welcome to your profile page</h1>
+            <div  style={p}>
                 tuner
             </div>
-            <div>
+            <div style={chat}>
                 chat
             </div>
             <Profile/>
