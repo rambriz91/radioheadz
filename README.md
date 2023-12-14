76c5d0adba26a05510522cbcce073b8249eff9e1
Sal Ceja <salceja45@gmail.com>
2023-12-13 18:14:52 -0800

    created readme file

--- README.md
+++ README.md
@@ -1,19 +1,34 @@
-# mern-project-template
+# <Radioheadz>
 
 ## Description
 
-Use this template to quickly bootstrap a MERN stack application.
+- We embarked on this project to address a gap in related apps. In this application, users can search for stations by city. Once the desired city is found, users have the option to select stations that might not be available in other applications. Constructing this application presented challenges, particularly in setting up the backend. As new developers, we sought guidance when faced with coding obstacles. Despite this challenge, we learned how to collaborate more effectively as a team. We adopted a team style that leveraged each member's strengths.
 
-## Instructions
+## Installation
 
-- Clone this repository with `git clone git@github.com:joshtaylor0404/mern-project-template.git`.
-- Copy the source files into a new folder with the name of your project.
-- If the `.git` folder was copied along with the files, delete it.
-- Initialize the new repository with the `git init` command.
-- Replace this `README.md` with your own.
-- Change the project name in `package.json` to the name of your project.
-- Change the database name in `/server/config/connection.js` from `mern-project-template` to the name of your project.
-- Commit all of the changes with `git add .` and `git commit -m "initial commit"`.
-- Create a new repository on GitHub with the same name as your project (Do not add a `README.md` or `.gitignore` file)
-- Use the script for pushing an existing repository to GitHub (second option) to connect your local repository with GitHub.
-- Start coding!
+- Installing this app is relatively simple. Visit our provided repository using the links at the bottom. Select the project repository and clone it to your local environment. To run the application, open the file with your code editor, perform an npm i to install all required dependencies, and run npm run develop in the terminal to view the app running on your local machine.
+
+## Usage
+
+- To use the application, follow the provided link below. Clicking it will direct you to the application. Once there, you can log in or sign up, depending on whether you have an existing account.
+  ![Alt text](client/public/images/readme-images/login.png)
+  ![Alt text](client/public/images/readme-images/signup.png)
+- Upon login, you'll reach the homepage featuring the radio tuner. Here, you can select a city and cycle through existing radio stations. Additionally, you can add stations to your favorites, populating the user's favorite preset buttons.
+  ![Alt text](client/public/images/readme-images/presets.png)
+- Subsequently, scroll through the stations, choose one, and hit the play button to start streaming audio for as long as you like.
+  ![Alt text](client/public/images/readme-images/tuner.png)
+- Finally, an important feature for the application's future is the option to donate for ongoing development. This link will direct you to a new page, providing an opportunity to support our ongoing efforts. When finished, simply log out.
+  ![Alt text](client/public/images/readme-images/donate.png)
+
+## Repo:
+
+- GitHub Repo: https://github.com/Sal1316/radioheadz
+
+## Credits
+
+- Collaborators:
+  - https://github.com/Sal1316
+  - https://github.com/rambriz91
+  - https://github.com/1014jorge
+  - https://github.com/Marc01710
+  - https://github.com/makandcheese04
--- client/src/components/Tuner.jsx
+++ client/src/components/Tuner.jsx
@@ -1,7 +1,7 @@
-import { useMutation } from '@apollo/client';
-import DropDown from './DropDown';
-import AudioPlayer from './AudioPlayer';
-import { UPDATE_FAVSTATION } from '../utils/mutations';
+import { useMutation } from "@apollo/client";
+import DropDown from "./DropDown";
+import AudioPlayer from "./AudioPlayer";
+import { UPDATE_FAVSTATION } from "../utils/mutations";
 
 const Tuner = ({
   stations,
@@ -29,15 +29,15 @@ const Tuner = ({
   const steps = Object.keys(stationData);
 
   const handleFavStation = async (event) => {
-    const stationId = event.target.closest('button').getAttribute('data-id');
+    const stationId = event.target.closest("button").getAttribute("data-id");
     try {
       const { data } = await updateFavStation({
         variables: { stationId },
       });
-      console.log('🥷🐢');
+      console.log("🥷🐢");
     } catch (error) {
       console.error(error);
-      console.log('Mutation Failed!');
+      console.log("Mutation Failed!");
     }
   };
 
@@ -85,67 +85,67 @@ const Tuner = ({
   };
 
   return (
-    <div className='p-3 rounded-md bg-[#2d3033] max-w-screen-lg flex flex-col min-w-[80%]'>
-      <div className='flex justify-between'>
+    <div className="p-3 rounded-md bg-[#2d3033] max-w-screen-lg flex flex-col min-w-[50%]">
+      <div className="flex justify-between">
         <DropDown onSelectCity={handleCitySelect} />
         {currentStation._id === null ? (
           <button
-            className='rounded-md bg-[#505458] hover:bg-gray-50'
+            className="rounded-md bg-[#505458] hover:bg-gray-50"
             onClick={handleFavStation}
           >
-            <i className='fa-solid fa-heart fa-xl text-[#db0d06]'></i>
+            <i className="fa-solid fa-heart fa-xl text-[#db0d06]"></i>
           </button>
         ) : (
           <button
-            className='rounded-md bg-[#505458] hover:bg-gray-50'
+            className="rounded-md bg-[#505458] hover:bg-gray-50"
             onClick={handleFavStation}
             data-id={currentStation._id}
           >
-            <i className='fa-solid fa-heart fa-xl text-[#db0d06]'></i>
+            <i className="fa-solid fa-heart fa-xl text-[#db0d06]"></i>
           </button>
         )}
       </div>
       <img
         src={
-          currentCity === 'San Diego'
-            ? '/images/sandiegoSidebar.png'
-            : currentCity === 'Los Angeles'
-            ? '/images/Los-Angeles.png'
-            : '/images/radioheadz-high-resolution-logo-transparent.png'
+          currentCity === "San Diego"
+            ? "/images/sandiegoSidebar.png"
+            : currentCity === "Los Angeles"
+            ? "/images/Los-Angeles.png"
+            : "/images/radioheadz-high-resolution-logo-transparent.png"
         }
         alt={currentCity}
       />
-      <h2 className='text-[#ffa62e] font-oleo-script flex justify-center p-1 m-2'>
-        {currentStation ? currentStation.name : 'Station Name'}
+      <h2 className="text-[#ffa62e] font-oleo-script flex justify-center p-1 m-2">
+        {currentStation ? currentStation.name : "Station Name"}
       </h2>
-      <div className='flex justify-around p-4'>
-        <button className='bg-transparent' onClick={handleTuneLeft}>
-          <i className='fa-solid fa-angles-left fa-2xl text-[#ffa62e] hover:text-[#ff622e]'></i>
+      <div className="flex justify-around p-4">
+        <button className="bg-transparent" onClick={handleTuneLeft}>
+          <i className="fa-solid fa-angles-left fa-2xl text-[#ffa62e] hover:text-[#ff622e]"></i>
         </button>
-        <h2 className='text-[#ff622e] font-oleo-script flex'>{freq}</h2>
-        <button className='bg-transparent' onClick={handleTuneRight}>
-          <i className='fa-solid fa-angles-right fa-2xl text-[#ffa62e] hover:text-[#ff622e]'></i>
+        <h2 className="text-[#ff622e] font-oleo-script flex">{freq}</h2>
+        <button className="bg-transparent" onClick={handleTuneRight}>
+          <i className="fa-solid fa-angles-right fa-2xl text-[#ffa62e] hover:text-[#ff622e]"></i>
         </button>
       </div>
-      <h3 className='text-[#ff622e] flex justify-center font-mono'>
-        {currentStation ? currentStation.callSign : 'Call Sign'}
+      <h3 className="text-[#ff622e] flex justify-center font-mono">
+        {currentStation ? currentStation.callSign : "Call Sign"}
       </h3>
-      <h4 className='text-[#ff622e] flex justify-center font-mono'>
-        {currentStation ? currentStation.genre : 'Genre'}
+      <h4 className="text-[#ff622e] flex justify-center font-mono">
+        {currentStation ? currentStation.genre : "Genre"}
       </h4>
       <AudioPlayer currentStation={currentStation} />
-      <div id='tuner-container'>
+      <div id="tuner-container">
         <input
-          className='w-full'
+          className="w-full"
           value={steps.indexOf(freq.toString())}
           onInput={handleTuner}
-          step='1'
-          type='range'
+          step="1"
+          type="range"
           min={0}
           max={steps.length - 1}
-          list='markers'
+          list="markers"
         />
-        <datalist id='markers'>
+        <datalist id="markers">
           {steps.map((step) => (
             <option key={step} value={step}>
               {step}
