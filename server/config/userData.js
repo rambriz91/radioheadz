126ddff1c667d1ffd0033b6620a2e153afb38e12
Sal Ceja <salceja45@gmail.com>
2023-12-14 15:19:38 -0800

    setup before deploying app

--- server/config/userData.js
+++ server/config/userData.js
@@ -1,53 +1,53 @@
 const userData = [
-    {
-        firstName: 'Pamela',
-        lastName: 'Washington',
-        username: 'Pwash',
-        email: 'pamela@testmail.com',
-        password: 'password12345',
-      },
-      {
-        firstName: 'Elijah',
-        lastName: 'Holt',
-        username: 'Eholt',
-        email: 'eholt@testmail.com',
-        password: 'password12345',
-      },
-      {
-        firstName: 'Jorge',
-        lastName: 'Gonzalez',
-        username: 'Jgonzalez',
-        email: 'Jgonzalez@testmail.com',
-        password: 'password123',
-      },
-      {
-        firstName: 'Rob',
-        lastName: 'Ambriz',
-        username: 'Rambriz',
-        email: 'Rambriz@testmail.com',
-        password: 'password123',
-      },
-      {
-        firstName: 'Marc',
-        lastName: 'Hernandez',
-        username: 'Mhernandez',
-        email: 'Mhernandez@testmail.com',
-        password: 'password123',
-      },
-      {
-        firstName: 'Sal',
-        lastName: 'Ceja',
-        username: 'Sceja',
-        email: 'Sceja@testmail.com',
-        password: 'password123',
-      },
-      {
-        firstName: 'Mak',
-        lastName: 'Russel',
-        username: 'Mrussel',
-        email: 'Mrussel@testmail.com',
-        password: 'password123',
-      }
-]
+  {
+    firstName: "Pamela",
+    lastName: "Washington",
+    username: "Pwash",
+    email: "pamela@testmail.com",
+    password: "password12345",
+  },
+  {
+    firstName: "Elijah",
+    lastName: "Holt",
+    username: "Eholt",
+    email: "eholt@testmail.com",
+    password: "password12345",
+  },
+  {
+    firstName: "Jorge",
+    lastName: "Gonzalez",
+    username: "Jgonzalez",
+    email: "Jgonzalez@testmail.com",
+    password: "password123",
+  },
+  {
+    firstName: "Rob",
+    lastName: "Ambriz",
+    username: "Rambriz",
+    email: "Rambriz@testmail.com",
+    password: "password123",
+  },
+  {
+    firstName: "Marc",
+    lastName: "Hernandez",
+    username: "Mhernandez",
+    email: "Mhernandez@testmail.com",
+    password: "password123",
+  },
+  {
+    firstName: "Sal",
+    lastName: "Ceja",
+    username: "sceja",
+    email: "ceja@gmail.com",
+    password: "password",
+  },
+  {
+    firstName: "Mak",
+    lastName: "Russel",
+    username: "Mrussel",
+    email: "Mrussel@testmail.com",
+    password: "password123",
+  },
+];
 
 module.exports = userData;
