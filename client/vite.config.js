5a15eb3ee2f1af2786caa3c455ade8cd7a1e1dc5
rambriz91 <rambriz91@gmail.com>
2024-01-10 12:13:01 -0800

    added dependencies.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,12 +11,12 @@ export default defineConfig({
       '/graphql': {
         target: 'http://localhost:3001',
         secure: false,
-        changeOrigin: true
-      }
-    }
+        changeOrigin: true,
+      },
+    },
   },
   test: {
     globals: true,
-    environment: 'happy-dom'
-  }
-})
+    environment: 'happy-dom',
+  },
+});
