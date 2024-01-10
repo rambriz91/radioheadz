8ec833e45ea1a36fdee48d5c8d3ea2024505b8e0
rambriz91 <rambriz91@gmail.com>
2024-01-10 15:14:58 -0800

    updating env variables.

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-    'mongodb+srv://rambriz91:*****@cluster0.dhjcdjc.mongodb.net/'
+    `mongodb+srv://rambriz91:HQryejMRrxFNVi7o@cluster0.dhjcdjc.mongodb.net/`
 );
 
 module.exports = mongoose.connection;
