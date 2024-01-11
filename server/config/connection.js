59141ea46280e4558ab9f71b4c357a16bbfa914e
rambriz91 <rambriz91@gmail.com>
2024-01-10 16:42:12 -0800

    stored password in env variable.

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-    `mongodb+srv://rambriz91:${DB_PASSWORD}@cluster0.dhjcdjc.mongodb.net/`
+    `mongodb+srv://rambriz91:${process.env.DB_PASSWORD}@cluster0.dhjcdjc.mongodb.net/`
 );
 
 module.exports = mongoose.connection;
